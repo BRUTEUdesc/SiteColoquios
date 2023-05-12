03276f62de5de696ca1ccbb6ac704d81703f587e
EMachad0 <elitonmachadod200@gmail.com>
2023-05-12 17:05:23 -0300

    remove url

--- app.py
+++ app.py
@@ -26,8 +26,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
-        DB_URL=os.environ.get('DB_URL'),
         DB_NAME=os.environ.get('DB_NAME'),
+        DB_HOST=os.environ.get('DB_HOST'),
+        DB_PORT=os.environ.get('DB_PORT'),
+        DB_USER=os.environ.get('DB_USER'),
+        DB_PASSWORD=os.environ.get('DB_PASSWORD'),
     )
 
     login_manager.init_app(app)
