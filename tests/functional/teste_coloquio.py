bc92bcda7756b7b99ededfc5c605c915bae5e57b
korhal <vitynho.moresco@gmail.com>
2023-06-01 17:46:22 -0300

    Teste coloquio render

---
+++ tests/functional/teste_coloquio.py
@@ -0,0 +1,24 @@
+import pytest
+
+from app.extensions.database import get_db
+
+
+class TesteColoquio:
+    @pytest.fixture
+    def create_coloquio(self, app):
+        coloquio_id = None
+        with app.app_context():
+            db = get_db()
+            with db.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO coloquios.apresentacao (titulo, datacol) VALUES ('Coloquio 1', '2019-12-12') "
+                    "RETURNING id;"
+                )
+                coloquio_id = cursor.fetchone()[0]
+                db.commit()
+        return coloquio_id
+
+    def test_render(self, admin_client, create_coloquio):
+        response = admin_client.get(f"/coloquios/{create_coloquio}", follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Coloquio 1' in response.data
